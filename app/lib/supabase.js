e60e62e0991d481dbe38e3c515b28ce38d5764fa
Aymen Cherkani <cherkaniaymen1@gmail.com>
2025-01-12 04:35:56 +0100

    front

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,11 +1,12 @@
 import "react-native-url-polyfill/auto";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://gsczqwlavnrthmrjhxxk.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzY3pxd2xhdm5ydGhtcmpoeHhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY2Mjg0OTAsImV4cCI6MjA1MjIwNDQ5MH0.l6gefouMZq2roXAwWzvyj7Mey99hXK0012WJKKbcOlo";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdzY3pxd2xhdm5ydGhtcmpoeHhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzY2Mjg0OTAsImV4cCI6MjA1MjIwNDQ5MH0.l6gefouMZq2roXAwWzvyj7Mey99hXK0012WJKKbcOlo";
 
-console.log('Creating Supabase client...');
+console.log("Creating Supabase client...");
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: AsyncStorage,
@@ -14,4 +15,4 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     detectSessionInUrl: false,
   },
 });
-console.log('Supabase client created:', supabase);
+console.log("Supabase client created:", supabase);
